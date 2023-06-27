769f1ae6d7e1528fbb1e89891e6ee96250aa2388
gadriv <grivera@gurmab.com>
2023-06-26 19:08:10 -0600

    Cambios en de la clase 3

--- src/PruebaMetodos.java
+++ src/PruebaMetodos.java
@@ -7,7 +7,38 @@ public class PruebaMetodos {
 		
 		miCuenta.saldo = 300;
 		
-		miCuenta.depositar(300);
+		miCuenta.depositar(200);
+		
+		System.out.println(miCuenta.saldo);
+		
+		miCuenta.retirar(100);
+		
+		//Primera parte
+		System.out.println();
+		System.out.println(miCuenta.saldo);
+		
+		//Segunda parte
+		
+		cuenta otraCuenta = new cuenta();
+	
+		otraCuenta.saldo = 1000;
+		
+		boolean transferencia = otraCuenta.transferir(400, miCuenta);
+		
+		System.out.println();
+		if (transferencia) {
+			System.out.println("Transferencia exitosa");
+		}else {
+			System.out.println("No fue posible");
+		}
+		
+		System.out.println();
+		System.out.println(otraCuenta.saldo);
+		System.out.println(miCuenta.saldo);
+		
+		
+		
+	
 	}
 
 }
--- src/cuenta.java
+++ src/cuenta.java
@@ -6,7 +6,37 @@ class cuenta{
 	int numero;
 	String titular;
 	
-	void depositar (double valor){ // Este metodo no retorna valor para este ejemplo
+	
+	//METODO PARA DEPORSITAR
+	// Este metodo no retorna valor para este ejemplo
+	public void depositar (double valor){ 
+	
+		this.saldo = this.saldo + valor;
+	}
+	
+	//METODO PARA RETIRAR
+	//Este metodo retorna valor
+	public boolean retirar (double valor){
+		if (this.saldo >= valor) {
+			this.saldo = this.saldo - valor;
+			
+			//tambien se pude colocar asi> this.saldo-=valor;
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	
+	//METODO PARA TRANSFERIR 
+	public boolean transferir (double valor, cuenta cuentaTranferir) {
+		
+		if (this.saldo>=valor) {
+			this.saldo-=valor;
+			cuentaTranferir.depositar(valor);
+			return true;
+			
+		}return false;
 		
 	}
 	
